aa108102e56872e28f2216ac7fbb2113d6d62bab
Miklovsh <miklovsh.vik@gmail.com>
2022-11-25 11:18:52 +0200

    1. Сonnected Axios 2.Created an API request and processed the received data 3.Created a context for passing data to child components 4.Created CurrencyRate component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,102 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import CurrencyRate from './CurrencyRate';
 import Exchange from './Exchange';
 import Footer from './Footer';
 import Context from './Context';
 import './App.css';
+import axios from 'axios';
 
 function App() {
-  return (
+  const [error, setError] = useState(null);
+  const [currentTime, setCurrentTime] = useState()
+  const [update, setUpdate] = useState();
+  const [usd, setUsd] = useState('');
+  const [eur, setEur] = useState('');
+  const [gbp, setGbp] = useState('');
+  const [cny, setCny] = useState('');
+  const [chf, setChf] = useState('');
+  const [aud, setAud] = useState('');
+  const [cad, setCad] = useState('');
+  const [czk, setCzk] = useState('');
+  const [pln, setPln] = useState('');
+  const [xau, setXau] = useState('');
 
-    <div className="App">
-      <Header />
-      <main className='main'>
-        <div className='container'>
-          <p className='main__сurrent-date'>Сьогодні в нас:</p>
-          <h2 className='main__update'>Дата останього оновлення:</h2>
-        <CurrencyRate />
-        <Exchange />
+
+  const url = `https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json`;
+
+
+  useEffect(() => {
+    try {
+      axios.get(url).then((response) => {
+        setUpdate(response.data[25].exchangedate);
+        setUsd(response.data[25].rate.toFixed(2));
+        setEur(response.data[32].rate.toFixed(2));
+        setGbp(response.data[24].rate.toFixed(2));
+        setCny(response.data[2].rate.toFixed(2));
+        setChf(response.data[22].rate.toFixed(2));
+        setAud(response.data[0].rate.toFixed(2));
+        setCad(response.data[1].rate.toFixed(2));
+        setCzk(response.data[4].rate.toFixed(2));
+        setPln(response.data[33].rate.toFixed(2));
+        setXau(response.data[58].rate.toFixed(2));
+        // console.log(response.data);
+      })
+    } catch {
+      setError('Виникла помилка, спробуйте трохи пізніше!');
+    }
+  }, [])
+
+
+
+  useEffect(() => {
+    setInterval(() => {
+      const date = new Date();
+      setCurrentTime(date.toLocaleTimeString());
+    }, 1000);
+  }, []);
+
+
+  const date = new Date();
+  const day = date.getDate();
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+
+
+  const value = {
+    usd,
+    eur,
+    cny,
+    gbp,
+    chf,
+    aud,
+    cad,
+    czk,
+    pln,
+    xau,
+  }
+  if (error) {
+    return <div>{error}</div>;
+  } else {
+    return (
+      <Context.Provider value={value}>
+        <div className="App">
+          <Header />
+          <main className='main'>
+            <div className='container'>
+              <p className='main__сurrent-date'>Сьогодні в нас: {day}.{month}.{year} {currentTime}</p>
+              <h2 className='main__update'>Дата останього оновлення: {update} </h2>
+              <CurrencyRate />
+              <Exchange />
+            </div>
+          </main>
+          <Footer />
         </div>
-      </main>
-      <Footer />
-    </div>
-  );
+      </Context.Provider>
+    );
+  }
 }
+
+
 
 export default App;
--- src/CurrencyRate.js
+++ src/CurrencyRate.js
@@ -1,26 +1,31 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import Context from "./Context";
 
 function CurrencyRate() {
-  return(
+
+  const value = useContext(Context);
+
+
+  return (
     <div className="CurrencyRate">
-  <div className='main__currency-inner'>
-            <div className='main__currency-box'>
-              <h3 className='main__currency'>Долар</h3>
-              <h4 className='main__currency-value'>40 гривень</h4>
-            </div>
-            <div className='main__currency-box'>
-              <h3 className='main__currency'>Євро</h3>
-              <h4 className='main__currency-value'>40 гривень</h4>
-            </div>
-            <div className='main__currency-box'>
-              <h3 className='main__currency'>Фунт стерлінгів</h3>
-              <h4 className='main__currency-value'>40 гривень</h4>
-            </div>
-            <div className='main__currency-box'>
-              <h3 className='main__currency'>Юань</h3>
-              <h4 className='main__currency-value'>40 гривень</h4>
-            </div>
-          </div>
+      <div className='main__currency-inner'>
+        <div className='main__currency-box'>
+          <h3 className='main__currency'>Долар</h3>
+          <h4 className='main__currency-value'>{value.usd} грн</h4>
+        </div>
+        <div className='main__currency-box'>
+          <h3 className='main__currency'>Євро</h3>
+          <h4 className='main__currency-value'>{value.eur} грн</h4>
+        </div>
+        <div className='main__currency-box'>
+          <h3 className='main__currency'>Фунт стерлінгів</h3>
+          <h4 className='main__currency-value'>{value.gbp} грн</h4>
+        </div>
+        <div className='main__currency-box'>
+          <h3 className='main__currency'>Юань</h3>
+          <h4 className='main__currency-value'>{value.cny} грн</h4>
+        </div>
+      </div>
     </div>
   )
 }
